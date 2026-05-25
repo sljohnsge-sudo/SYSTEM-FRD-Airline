a180a21c8d2e45aa5ce83528c74dd19bbaefaf1c
sljohnsge-sudo <sljohnsge@gmail.com>
2026-05-25 16:53:43 +0530

    new

--- app.py
+++ app.py
@@ -1977,8 +1977,3 @@ def api_agent_credit():
 
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1", port=5000)
-
-@app.route('/logout')
-def logout():
-    session.clear()
-    return redirect(url_for('index'))
